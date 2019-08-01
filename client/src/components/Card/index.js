2c74500ee19a02bfcf9f6e25191cc4ec2859eedf
Phillip Chang <phillip-chang@live.com>
2019-07-31 19:16:49 -0700

    Change favicon and add comma to seperate multiple authors

--- client/src/components/Card/index.js
+++ client/src/components/Card/index.js
@@ -5,7 +5,7 @@ export function Card(props) {
     return (
         <div className="card" >
             <h3 className="card-title">{props.title}</h3>
-            <h5>Written by:&nbsp;{props.author}&nbsp;</h5>
+            <h5>Written by:&nbsp;{props.author.join(", ")}&nbsp;</h5>
             <img src={props.thumbnail} className="card-img-top" alt={props.title} />
             <div className="card-body">
 
@@ -21,7 +21,7 @@ export function CardSaved(props) {
     return (
         <div className="card" >
             <h3 className="card-title">{props.title}</h3>
-            <h5>Written by:&nbsp;{props.author}</h5>
+            <h5>Written by:&nbsp;{props.author.join(", ")}</h5>
             <img src={props.thumbnail} className="card-img-top" alt={props.title} />
             <div className="card-body">
 
